d214ce884518faa0faac01645fff6d4afd2211d1
Alejo Boga <alejoboga20@gmail.com>
2023-02-17 15:32:14 -0500

    use useId hook

--- src/components/users/LabelInput.tsx
+++ src/components/users/LabelInput.tsx
@@ -1,15 +1,19 @@
-import { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes, useId } from 'react';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  label: string;
-  value: string;
+	label: string;
+	value: string;
 }
 
 export function LabelInput({ label, value, ...rest }: Props) {
-  return (
-    <div className="mb-3">
-      <label className="form-label">{label}</label>
-      <input className="form-control" value={value} {...rest} />
-    </div>
-  );
+	const inputId = useId();
+
+	return (
+		<div className='mb-3'>
+			<label className='form-label' htmlFor={inputId}>
+				{label}
+			</label>
+			<input className='form-control' id={inputId} value={value} {...rest} />
+		</div>
+	);
 }
