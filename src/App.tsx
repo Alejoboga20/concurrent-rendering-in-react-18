cc331206d73c562dc66332c67590a499088bc0c7
Alejo Boga <alejoboga20@gmail.com>
2023-02-17 14:47:22 -0500

    Nest suspense

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { AppRoutes } from './components/AppRoutes';
 import { NavBar } from './components/NavBar';
+import { Suspense } from 'react';
+import { Loading } from './components/Loading';
 
 function App() {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <NavBar />
-        <AppRoutes />
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div className='container'>
+			<BrowserRouter>
+				<NavBar />
+				<Suspense fallback={<Loading />}>
+					<AppRoutes />
+				</Suspense>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
--- src/components/users/UserDetails.tsx
+++ src/components/users/UserDetails.tsx
@@ -14,15 +14,17 @@ export function UserDetails({ userId, movieId }: Props) {
 		<div>
 			<h4 className='text-center mt-5'>User details</h4>
 			<Suspense fallback={<Loading />}>
-				<ErrorBoundary FallbackComponent={ErrorFallback}>
-					<AccountDetails userId={userId} />
-				</ErrorBoundary>
-			</Suspense>
-			<h4 className='text-center mt-5'>Favorite movie</h4>
-			<Suspense fallback={<Loading />}>
-				<ErrorBoundary FallbackComponent={ErrorFallback}>
-					<MovieDetails movieId={movieId} />
-				</ErrorBoundary>
+				<Suspense fallback={<Loading />}>
+					<ErrorBoundary FallbackComponent={ErrorFallback}>
+						<AccountDetails userId={userId} />
+					</ErrorBoundary>
+				</Suspense>
+				<h4 className='text-center mt-5'>Favorite movie</h4>
+				<Suspense fallback={<Loading />}>
+					<ErrorBoundary FallbackComponent={ErrorFallback}>
+						<MovieDetails movieId={movieId} />
+					</ErrorBoundary>
+				</Suspense>
 			</Suspense>
 		</div>
 	);
