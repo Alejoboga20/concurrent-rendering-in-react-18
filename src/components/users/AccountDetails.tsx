49739d326abc37148b4712c2eeb71023d6a5cca0
Alejo Boga <alejoboga20@gmail.com>
2023-02-17 14:30:48 -0500

    remove loading state in components

--- src/components/users/AccountDetails.tsx
+++ src/components/users/AccountDetails.tsx
@@ -1,28 +1,21 @@
 import useSWR from 'swr';
 import type { Account } from '../../types';
-import { Loading } from '../Loading';
 import { LabelInput } from './LabelInput';
 interface Props {
-  userId: number;
+	userId: number;
 }
 export function AccountDetails({ userId }: Props) {
-  const { data: account, error } = useSWR<Account, Error>(
-    `${process.env.REACT_APP_API_BASE}/accounts/${userId}?sleep=1000`
-  );
+	const { data } = useSWR<Account, Error>(
+		`${process.env.REACT_APP_API_BASE}/accounts/${userId}?sleep=1000`
+	);
 
-  if (error) {
-    return <div>{error.message}</div>;
-  }
+	const account = data!;
 
-  if (!account) {
-    return <Loading />;
-  }
-
-  return (
-    <div className="row">
-      <LabelInput label="Firstname" value={account.firstname} readOnly />
-      <LabelInput label="Surname" value={account.surname} readOnly />
-      <LabelInput label="Email address" value={account.email} readOnly />
-    </div>
-  );
+	return (
+		<div className='row'>
+			<LabelInput label='Firstname' value={account.firstname} readOnly />
+			<LabelInput label='Surname' value={account.surname} readOnly />
+			<LabelInput label='Email address' value={account.email} readOnly />
+		</div>
+	);
 }
--- src/components/users/MovieDetails.tsx
+++ src/components/users/MovieDetails.tsx
@@ -1,35 +1,23 @@
 import useSWR from 'swr';
 import type { Movie } from '../../types';
-import { Loading } from '../Loading';
 import { LabelInput } from './LabelInput';
 import { LabelTextarea } from './LabelTextarea';
 
 interface Props {
-  movieId: number;
+	movieId: number;
 }
 export function MovieDetails({ movieId }: Props) {
-  const { data: movie, error } = useSWR<Movie, Error>(
-    `${process.env.REACT_APP_API_BASE}/top-rated-movies/${movieId}?sleep=500`
-  );
+	const { data } = useSWR<Movie, Error>(
+		`${process.env.REACT_APP_API_BASE}/top-rated-movies/${movieId}?sleep=500`
+	);
 
-  if (error) {
-    return <div>{error.message}</div>;
-  }
+	const movie = data!;
 
-  if (!movie) {
-    return <Loading />;
-  }
-
-  return (
-    <div className="row">
-      <LabelInput label="Title" value={movie.title} readOnly />
-      <LabelInput label="Release date" value={movie.release_date} readOnly />
-      <LabelTextarea
-        label="Overview"
-        value={movie.overview}
-        readOnly
-        rows={5}
-      />
-    </div>
-  );
+	return (
+		<div className='row'>
+			<LabelInput label='Title' value={movie.title} readOnly />
+			<LabelInput label='Release date' value={movie.release_date} readOnly />
+			<LabelTextarea label='Overview' value={movie.overview} readOnly rows={5} />
+		</div>
+	);
 }
